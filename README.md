d2a07046a75588e818b2b9a05c81fe1e3d1a505a
Jacob <jacobrobinsoncoleman@gmail.com>
2022-05-09 13:38:16 -0400

    Test Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Final APCS project started on on 5/6/2022. Time of writing this README is 2:33
+
+DUE JUNE 10TH(?)
---
+++ src/Constants.java
@@ -0,0 +1,13 @@
+public class Constants {
+    enum cardValue {
+        ACE, KING, QUEEN, JACK, TEN, NINE, EIGHT, SEVEN, SIX, FIVE, FOUR, THREE, TWO;
+    }
+
+    enum cardSet {
+        HEART, DIAMOND, SPADE, CLUB;
+    }
+
+    public class mainBlackjack {
+        public final int NUMOFDECKS = 1;
+    }
+}
