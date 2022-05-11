089c9b25fc137261e5f18411d62b749c6b36a251
Jacob <jacobrobinsoncoleman@gmail.com>
2022-05-11 14:58:37 -0400

    Got rid of all the test code and moved to blackJack so the runner only is the runner - Jacob

--- src/blackjackRunner.java
+++ src/blackjackRunner.java
@@ -1,14 +1,7 @@
 import javax.swing.*;
-import javax.xml.stream.events.StartDocument;
 public class blackjackRunner {
     public static void main(String[] args) {
-        public 
-        JFrame frame = new JFrame(Constants.mainBlackjack.NAME);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(Constants.mainBlackjack.WIDTH, Constants.mainBlackjack.HEIGHT);
-        JButton start = new JButton("Start");
-        frame.getContentPane().add(start);
-        frame.setVisible(Constants.mainBlackjack.VISFRAME);
-        start.action(evt, what)
+        blackjack jack = new blackjack();
+        jack.startup();
     }
 }
