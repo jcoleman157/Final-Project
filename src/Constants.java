41e9fdf756f7d3b3841c0b6daea4189e55346794
Jacob <jacobrobinsoncoleman@gmail.com>
2022-05-11 14:57:48 -0400

    Seeing how the swing library actually works and how to make buttons work (they dont) - Jacob

--- src/Constants.java
+++ src/Constants.java
@@ -1,22 +1,27 @@
 public class Constants {
-        //  what type of card it is so we can count cards while playing
+    // what type of card it is so we can count cards while playing
     public enum cardValue {
         ACE, KING, QUEEN, JACK, TEN, NINE, EIGHT, SEVEN, SIX, FIVE, FOUR, THREE, TWO;
     }
-        // what type of card it is so we can count cards while playing
+
+    // what type of card it is so we can count cards while playing
     public enum cardSet {
         HEART, DIAMOND, SPADE, CLUB;
     }
 
     public class mainBlackjack {
-        // the number of decks used for counting cards. default is one so one of each set will show up. if 2 than 2 will show
+        // the number of decks used for counting cards. default is one so one of each
+        // set will show up. if 2 than 2 will show
         public final int NUMOFDECKS = 1;
-        // What the name of the box that pops up is. Should be what it is you're looking at. (may change to "Cardgame")
-        public static final String NAME = "BLACKJACK";
-        // Dimentions for the frame 
-        public static final int WIDTH = 800;
-        public static final int HEIGHT = 800;
+        // Dimentions for the frame
+        public static final int WIDTH = 0;
+        public static final int HEIGHT = 0;
         // Swing for some reason requires a bool to see if frame is visible. ALWAYS TRUE
         public static final boolean VISFRAME = true;
+        // What the name of the box that pops up is. Should be what it is you're looking
+        // at. (may change to "Cardgame")
+        public static final String NAME = "BLACKJACK";
+        // This is the text for the start button
+        public static final String START = "Start";
     }
 }
