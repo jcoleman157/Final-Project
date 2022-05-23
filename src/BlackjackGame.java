5577d31ee0f361ce821aeeac450cc44f86589d06
Jacob <jacobrobinsoncoleman@gmail.com>
2022-05-23 12:17:45 -0400

    Worked on making the dealer hand and making it so ace will switch between 1 and 11

--- src/BlackjackGame.java
+++ src/BlackjackGame.java
@@ -3,7 +3,10 @@ import java.util.*;
 
 public class BlackjackGame {
     private int numOfDecks;
+    private int userCount = 0;
+    private int dealerCount = 0;
     private int money;
+    private int aceCounter = 0;
     private ArrayList<Cards> arrList;
 
     public BlackjackGame() {
@@ -30,38 +33,40 @@ public class BlackjackGame {
      * This is the "logic" for the game
      */
     public void gameLogic() {
-        int userCount = 0;
         boolean keepPlaying = true;
         getNewDeck();
         shuffleDeck();
         int bet = userBet();
         Cards currentCard = drawCard();
+        Cards dealerCard = drawCard();
+        printDealerCard(dealerCard);
+        printCard(currentCard);
         Constants.cardValue cardVal = currentCard.getCardValue();
-        userCount += userCardValue(cardVal);
+        Constants.cardValue dealerCardVal = dealerCard.getCardValue();
+        dealerCount += numberValue(dealerCardVal);
+        userCount += numberValue(cardVal);
         currentCard = drawCard();
         cardVal = currentCard.getCardValue();
-        userCount += userCardValue(cardVal);
+        userCount += numberValue(cardVal);
         while (keepPlaying) {
+            printCard(currentCard);
             System.out.println("User hand : " + userCount);
             keepPlaying = hitOrStay();
             if (keepPlaying) {
                 currentCard = drawCard();
                 cardVal = currentCard.getCardValue();
-                userCount += userCardValue(cardVal);
-                if(userCount > 21){
-                    keepPlaying = false;
-                    addAndSubtractMoney(false, bet);
-                    System.out.println("You bust! You lost " + bet);
-                }
+                userCount += numberValue(cardVal);
+                keepPlaying = userBust(bet);
             }
         }
         if(keepPlaying()){
+            userCount = 0;
             gameLogic();
         }
     }
 
     /**
-     * This will draw 52 * numOfCards cards
+     * This will draw 52 * numOfDecks cards
      */
     private void getNewDeck() {
         for (int g = 1; g <= numOfDecks; g++) {
@@ -88,10 +93,10 @@ public class BlackjackGame {
         for (int i = 0; i < 10; i++) {
             System.out.println();
         }
-        // for (int i = 0; i < arrList.size(); i++){
-        // System.out.println("Card : Value = " + arrList.get(i).getCardValue() + ",
-        // Suite = " + arrList.get(i).getCardSet());
-        // }
+        for (int i = 0; i < arrList.size(); i++){
+        System.out.println("Card : Value = " + arrList.get(i).getCardValue() + 
+        ", Suite = " + arrList.get(i).getCardSet());
+         }
     }
 
     private Cards drawCard() {
@@ -102,9 +107,10 @@ public class BlackjackGame {
      * @param val - What the card value is
      * @return the value of the card
      */
-    private int userCardValue(Constants.cardValue val) {
+    private int numberValue(Constants.cardValue val) {
         if (val.equals(Constants.cardValue.ACE)) {
-            return userOneOrElev();
+            aceCounter++;
+            return oneOrElev();
         }
         if (val.equals(Constants.cardValue.KING) || val.equals(Constants.cardValue.QUEEN)
                 || val.equals(Constants.cardValue.JACK) || val.equals(Constants.cardValue.TEN)) {
@@ -137,14 +143,13 @@ public class BlackjackGame {
         return -1; // SHOULD be unreachable this is just in case, can never be too carful
     }
 
-    private int userOneOrElev() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("type Y for one, N for elevin");
-        String answerElevOrOne = scan.nextLine();
-        if (answerElevOrOne.charAt(0) == 'y' || answerElevOrOne.charAt(0) == 'Y') {
-            return 1;
-        } else
+    private int oneOrElev(){
+        if (userCount <= 10) {
             return 11;
+        }
+        else{
+            return 1;
+        }
     }
 
     private boolean hitOrStay() {
@@ -191,38 +196,41 @@ public class BlackjackGame {
             System.out.println("Push, no one lost");
         }
     }
-    private int dealerHand(Constants.cardValue val){
-        if (val.equals(Constants.cardValue.ACE)) {
-            return 11;
+    /**
+     * Checks if user busts
+     * @param bet - amount user bet
+     */
+    private boolean userBust(int bet){
+        aceChanger();
+            if(userCount > 21){
+            addAndSubtractMoney(false, bet);
+            System.out.println("You bust! You lost " + bet);
+            return false;
         }
-        if (val.equals(Constants.cardValue.KING) || val.equals(Constants.cardValue.QUEEN)
-                || val.equals(Constants.cardValue.JACK) || val.equals(Constants.cardValue.TEN)) {
-            return 10;
+        return true;
+    }
+    /**
+     * makes it so if user busts but they have an ace it will subtract 10
+     */
+    private void aceChanger(){
+        if(userCount > 21 && aceCounter != 0){
+            userCount -= 10;
+            aceCounter--;
         }
-        if (val.equals(Constants.cardValue.NINE)) {
-            return 9;
-        }
-        if (val.equals(Constants.cardValue.EIGHT)) {
-            return 8;
-        }
-        if (val.equals(Constants.cardValue.SEVEN)) {
-            return 7;
-        }
-        if (val.equals(Constants.cardValue.SIX)) {
-            return 6;
-        }
-        if (val.equals(Constants.cardValue.FIVE)) {
-            return 5;
-        }
-        if (val.equals(Constants.cardValue.FOUR)) {
-            return 4;
-        }
-        if (val.equals(Constants.cardValue.THREE)) {
-            return 3;
-        }
-        if (val.equals(Constants.cardValue.TWO)) {
-            return 2;
-        }
-        return -1; // SHOULD be unreachable this is just in case, can never be too carful
+    }
+    /**
+     * just prints the card
+     * @param card - card
+     */
+    private void printCard(Cards card){
+        System.out.println("Card : Value = " + card.getCardValue() + ", Suite = " + card.getCardSet()); 
+
+    }
+    /**
+     * just prints the dealers card
+     * @param card - dealers card
+     */
+    private void printDealerCard(Cards card){
+        System.out.println("Card : Value = " + card.getCardValue() + ", Suite = " + card.getCardSet()); 
     }
 }
